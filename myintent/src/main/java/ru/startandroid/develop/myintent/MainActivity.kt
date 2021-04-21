d9cb889881545fe09cbf7c2ff47ce60b63b0ea7d
leil25 <nuriaxmetova93@mail.ru>
2021-04-21 14:11:26 +0300

    Set Listener for Navigation Menu
    This Listener react on menu items click.

--- myintent/src/main/java/ru/startandroid/develop/myintent/MainActivity.kt
+++ myintent/src/main/java/ru/startandroid/develop/myintent/MainActivity.kt
@@ -2,14 +2,29 @@ package ru.startandroid.develop.myintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var nav_view = findViewById<NavigationView>(R.id.nav_view)
+        nav_view.setNavigationItemSelectedListener( this )
+    }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId)
+        {
+            R.id.id_fish -> Toast.makeText(this, "Id fish", Toast.LENGTH_SHORT).show()
+            R.id.id_nazh -> Toast.makeText(this, "Id nazhivka", Toast.LENGTH_SHORT).show()
+            R.id.id_sna -> Toast.makeText(this, "Id snasti", Toast.LENGTH_SHORT).show()
+            R.id.id_his -> Toast.makeText(this, "Id story", Toast.LENGTH_SHORT).show()
+        }
+
+        return true
     }
 }

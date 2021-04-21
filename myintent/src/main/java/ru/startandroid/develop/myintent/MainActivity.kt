eff3526d06ab7caa12de58caaf70d3c0a4495866
leil25 <nuriaxmetova93@mail.ru>
2021-04-21 11:44:38 +0300

    Create "Fishermen application"

--- myintent/src/main/java/ru/startandroid/develop/myintent/MainActivity.kt
+++ myintent/src/main/java/ru/startandroid/develop/myintent/MainActivity.kt
@@ -2,10 +2,14 @@ package ru.startandroid.develop.myintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }

63fed6a41f2b57c5c0d1beae026d5369cd42294b
leil25 <nuriaxmetova93@mail.ru>
2021-04-23 08:56:17 +0300

    Fill 'Compas' application module
    We create 'compas', which work in smartphones but don`t work in tablebooks.

--- mycompas/src/main/java/ru/startandroid/develop/mycompas/MainActivity.kt
+++ mycompas/src/main/java/ru/startandroid/develop/mycompas/MainActivity.kt
@@ -1,11 +1,47 @@
 package ru.startandroid.develop.mycompas
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    var manager:SensorManager? = null
+    var current_degree:Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        manager= getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+    override fun onResume() {
+        super.onResume()
+        manager?.registerListener(this, manager?.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_GAME)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        manager?.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        val degree:Int = event?.values?.get(0)?.toInt()!!
+        tvText.text = degree.toString()
+        val rotationAnim = RotateAnimation(current_degree.toFloat(), (-degree).toFloat(), Animation.RELATIVE_TO_SELF, 0.5f,
+                                            Animation.RELATIVE_TO_SELF, 0.5f)
+        rotationAnim.duration = 210
+        rotationAnim.fillAfter = true
+        current_degree = -degree
+        imgDynamic.startAnimation(rotationAnim)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
     }
 }

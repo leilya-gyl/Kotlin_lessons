fdce1d430505a891ee3f0c4996cc6573018e8f62
leil25 <nuriaxmetova93@mail.ru>
2021-04-23 08:58:15 +0300

    Create new content for 'Fisherman handbook'
    We use WebView object to show content-info.

--- myintent/src/main/java/ru/startandroid/develop/myintent/ContentActivity.kt
+++ myintent/src/main/java/ru/startandroid/develop/myintent/ContentActivity.kt
@@ -3,15 +3,18 @@ package ru.startandroid.develop.myintent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.*
+import kotlinx.android.synthetic.main.content2_layout.*
 import kotlinx.android.synthetic.main.content_layout.*
 
 class ContentActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.content_layout)
+        setContentView(R.layout.content2_layout)
 
-        myTitle.text = intent.getStringExtra("title")
+        /*myTitle.text = intent.getStringExtra("title")
         myContent.text = intent.getStringExtra("content")
-        im.setImageResource(intent.getIntExtra("image", R.drawable.som))
+        im.setImageResource(intent.getIntExtra("image", R.drawable.som))*/
+
+        webView.loadUrl("file:///android_asset/som.html")
     }
 }
